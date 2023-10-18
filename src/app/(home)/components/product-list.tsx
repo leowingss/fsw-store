be09c5848ee2ae5d1851ca04d1b901a128e1105a
Leonardo <leondani853@gmail.com>
2023-10-18 03:31:36 -0300

    feat: add page to show category product

--- src/app/(home)/components/product-list.tsx
+++ src/app/(home)/components/product-list.tsx
@@ -1,6 +1,6 @@
-import { Product } from "@prisma/client";
 import ProductItem from "@/components/ui/productItem";
 import { computeProductTotalPrice } from "@/helpers/product";
+import { Product } from "@prisma/client";
 
 interface ProductListProps {
   products: Product[];
@@ -8,12 +8,11 @@ interface ProductListProps {
 
 const ProductList = ({ products }: ProductListProps) => {
   return (
-    <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden ">
+    <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
       {products.map((product) => (
-        <ProductItem
-          key={product.id}
-          product={computeProductTotalPrice(product)}
-        />
+        <div key={product.id} className="w-[170px] max-w-[170px]">
+          <ProductItem product={computeProductTotalPrice(product)} />
+        </div>
       ))}
     </div>
   );
--- src/components/ui/productItem.tsx
+++ src/components/ui/productItem.tsx
@@ -1,5 +1,4 @@
 import { ProductWithTotalPrice } from "@/helpers/product";
-import { Product } from "@prisma/client";
 import Image from "next/image";
 import { Badge } from "./badge";
 import { ArrowDownIcon } from "lucide-react";
@@ -10,14 +9,14 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex max-w-[156px] flex-col gap-4">
-      <div className=" relative flex h-[170px] w-[156px]  items-center justify-center rounded-lg bg-accent">
+    <div className="flex flex-col gap-4">
+      <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
         <Image
           src={product.imageUrls[0]}
           height={0}
           width={0}
           sizes="100vw"
-          className="h-[90px] max-h-[70%] w-auto max-w-[80%]"
+          className="h-auto max-h-[70%] w-auto max-w-[80%]"
           style={{
             objectFit: "contain",
           }}
@@ -25,29 +24,30 @@ const ProductItem = ({ product }: ProductItemProps) => {
         />
 
         {product.discountPercentage > 0 && (
-          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+          <Badge className="absolute left-3 top-3 px-2 py-[2px] ">
             <ArrowDownIcon size={14} /> {product.discountPercentage}%
           </Badge>
         )}
       </div>
 
-      <div className="gap1 flex flex-col">
-        <p className=" w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+      <div className="flex flex-col gap-1">
+        <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
           {product.name}
         </p>
 
-        <div className="flex items-center gap-2 overflow-hidden text-ellipsis whitespace-nowrap">
+        <div className="flex items-center gap-2 ">
           {product.discountPercentage > 0 ? (
             <>
               <p className="overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
                 R$ {product.totalPrice.toFixed(2)}
               </p>
+
               <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
-                R$ {Number(product.basePrice.toFixed(2))}
+                R$ {Number(product.basePrice).toFixed(2)}
               </p>
             </>
           ) : (
-            <p className="text-sm font-semibold">
+            <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold">
               R$ {product.basePrice.toFixed(2)}
             </p>
           )}
