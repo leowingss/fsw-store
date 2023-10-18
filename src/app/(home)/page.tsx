a8875ef369ee65c97e7d0fced00eb3bf6b3169ef
Leonardo <leondani853@gmail.com>
2023-10-18 02:08:35 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -23,8 +23,18 @@ export default async function Home() {
       </div>
 
       <div className="mt-8">
+        <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src="/banner-home-02.png"
+        width={0}
+        height={0}
+        alt="Até 55% de desconto em mouses!"
+        className="h-auto w-full px-5"
+        sizes="100vw"
+      />
     </div>
   );
 }
