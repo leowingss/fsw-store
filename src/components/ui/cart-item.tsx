c5b29e9750fabf74d9c1c2c9f9ac00dd7e91649a
Leonardo <leondani853@gmail.com>
2023-10-19 03:35:03 -0300

    feat: ensure products can be removed from cart.

--- src/components/ui/cart-item.tsx
+++ src/components/ui/cart-item.tsx
@@ -9,15 +9,22 @@ interface CartItemProps {
 }
 
 const CartItem = ({ product }: CartItemProps) => {
-  const { decreaseProductQuantity, increaseProductQuantity } =
-    useContext(CartContext);
+  const {
+    decreaseProductQuantity,
+    increaseProductQuantity,
+    removeProductFromCart,
+  } = useContext(CartContext);
 
-  const handleDecreaseProductQuantity = () => {
+  const handleDecreaseProductQuantityClick = () => {
     decreaseProductQuantity(product.id);
   };
 
-  const handleIncreaseProductQuantity = () => {
+  const handleIncreaseProductQuantityClick = () => {
     increaseProductQuantity(product.id);
+  };
+
+  const handleRemoveProductClick = () => {
+    removeProductFromCart(product.id);
   };
 
   return (
@@ -52,7 +59,7 @@ const CartItem = ({ product }: CartItemProps) => {
               size={"icon"}
               variant={"outline"}
               className="h-8 w-8"
-              onClick={handleDecreaseProductQuantity}
+              onClick={handleDecreaseProductQuantityClick}
             >
               <ArrowLeftIcon size={16} />
             </Button>
@@ -61,14 +68,14 @@ const CartItem = ({ product }: CartItemProps) => {
               size={"icon"}
               variant={"outline"}
               className="h-8 w-8"
-              onClick={handleIncreaseProductQuantity}
+              onClick={handleIncreaseProductQuantityClick}
             >
               <ArrowRightIcon size={16} />
             </Button>
           </div>
         </div>
       </div>
-      <Button size="icon" variant="outline">
+      <Button size="icon" variant="outline" onClick={handleRemoveProductClick}>
         <TrashIcon size={16} />
       </Button>
     </div>
